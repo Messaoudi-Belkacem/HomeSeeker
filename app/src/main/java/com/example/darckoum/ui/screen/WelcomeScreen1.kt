d532c165aa777bd6f1c770cd259747cf2ee6e6f4
HP <wailmessaoudi806@gmail.com>
2024-03-04 22:32:15 +0100

    I improved some imports

--- app/src/main/java/com/example/darckoum/ui/screen/WelcomeScreen1.kt
+++ app/src/main/java/com/example/darckoum/ui/screen/WelcomeScreen1.kt
@@ -9,22 +9,16 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.darckoum.navigation.Screen
-import com.example.darckoum.ui.theme.C1
-import com.example.darckoum.ui.theme.C3
-import com.example.darckoum.ui.theme.C5
 
 @Composable
 fun WelcomeScreen1(
